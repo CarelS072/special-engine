edb7795980112b9447ad999de8ccccd0a891a1d4
CarelS072 <carelstarreveld@hotmail.com>
2019-01-24 23:07:28 +1300

    added router dom, axios, mock data

--- src/App.js
+++ src/App.js
@@ -4,7 +4,9 @@ import Header from './components/layout/header'
 import Todos from './components/Todos'
 import AddTodo from './components/AddTodo'
 import uuid from 'uuid'
-
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import About from './components/pages/About'
+import axios from 'axios'
 
 class App extends Component {
   state = {
@@ -26,7 +28,13 @@ class App extends Component {
       }
     ]
   }
+
+  componentDidMount() {
+    axios.get('https://jsonplaceholder.typicode.com/todos?_limit=10')
+      .then(res => this.setState( {todos: res.data }))
+  }
   
+
   markComplete = (id) => {
     this.setState({ todos: this.state.todos.map(todo => {
         if (todo.id === id) {
@@ -54,13 +62,21 @@ class App extends Component {
   render() {
     console.log(this.state.todos)
     return (
-      <div className="App">
-        <div className="container">
-          <Header />
-          <AddTodo addTodo={this.addTodo}/>
-          <Todos todos={this.state.todos} markComplete={this.markComplete} delTodo={this.delTodo} />
+      <Router>
+        <div className="App">
+          <div className="container">
+            <Header />
+            <Route exact path="/" render={props => (
+              <React.Fragment>
+                <AddTodo addTodo={this.addTodo}/>
+                <Todos todos={this.state.todos} markComplete={this.markComplete} delTodo={this.delTodo} />
+              </React.Fragment>
+            )} />
+            <Route path="/about" component={About} />
+          </div>
         </div>
-      </div>
+      </Router>
+      
     );
   }
 }
---
+++ src/components/pages/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function About() {
+  return (
+    <React.Fragment>
+      <h1>About</h1>
+      <p>
+        This is the Todolist app  v1.0 which is a part of a <a href='https://www.youtube.com/watch?v=sBws8MSXN7A&t=3s'>React crash course</a></p>
+    </React.Fragment>
+    
+  )
+}
