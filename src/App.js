d9a53a1c98aa7a2c2bdc4cfe0233ca03372d2ed8
CarelS072 <carelstarreveld@hotmail.com>
2019-01-24 21:57:05 +1300

    working list delete and complete

--- src/App.js
+++ src/App.js
@@ -22,12 +22,28 @@ class App extends Component {
       }
     ]
   }
+  
+  markComplete = (id) => {
+    this.setState({ todos: this.state.todos.map(todo => {
+        if (todo.id === id) {
+          todo.completed = !todo.completed
+        }
+        return todo
+      })
+    })
+  }
+
+  delTodo = (id) => {
+    this.setState({ todos: [...this.state.todos.filter(todo => todo.id !== id)]
+    })
+  }
+
   render() {
     console.log(this.state.todos)
     return (
       <div className="App">
           <h1>TODO app</h1>
-          <Todos todos={this.state.todos} />
+          <Todos todos={this.state.todos} markComplete={this.markComplete} delTodo={this.delTodo} />
       </div>
     );
   }
