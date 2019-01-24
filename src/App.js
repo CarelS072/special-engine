e38c5d39979e439de9e376cea68bc418aa77dd2d
CarelS072 <carelstarreveld@hotmail.com>
2019-01-24 22:41:26 +1300

    added styling, delete add todo and UUID

--- src/App.js
+++ src/App.js
@@ -1,22 +1,26 @@
 import React, { Component } from 'react';
 import './App.css';
+import Header from './components/layout/header'
 import Todos from './components/Todos'
+import AddTodo from './components/AddTodo'
+import uuid from 'uuid'
+
 
 class App extends Component {
   state = {
     todos: [
       {
-        id: 1,
+        id: uuid.v4(),
         title: 'Take out the trash',
         completed: false
       },
       {
-        id: 2,
+        id: uuid.v4(),
         title: 'Make trash',
         completed: false
       },
       {
-        id: 3,
+        id: uuid.v4(),
         title: 'Enjoy',
         completed: false
       }
@@ -38,12 +42,24 @@ class App extends Component {
     })
   }
 
+  addTodo = (title) => {
+    const newTodo = {
+      id: uuid.v4(),
+      title,
+      completed: false
+    }
+    this.setState({ todos: [...this.state.todos, newTodo] })
+  }
+
   render() {
     console.log(this.state.todos)
     return (
       <div className="App">
-          <h1>TODO app</h1>
+        <div className="container">
+          <Header />
+          <AddTodo addTodo={this.addTodo}/>
           <Todos todos={this.state.todos} markComplete={this.markComplete} delTodo={this.delTodo} />
+        </div>
       </div>
     );
   }
